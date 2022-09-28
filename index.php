3d64eb8451bfaa659a51c6d3281cc42b77ac0808
LalynMendoza1 <lalynmendoza0@gmail.com>
2022-09-28 23:55:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-phpinfo();
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Test App Lalyn</title>
+
+	<script type="text/javascript" charset="utf-8">
+		(function (g, e, n, es, ys) {
+			g['_genesysJs'] = e;
+			g[e] = g[e] || function () {
+			(g[e].q = g[e].q || []).push(arguments)
+			};
+			g[e].t = 1 * new Date();
+			g[e].c = es;
+			ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+		})(window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
+			environment: 'usw2',
+			deploymentId: '8a249d75-28a2-4c9b-b779-cd2943371db1'
+		});
+	</script>
+</head>
+
+<body>
+	<!-- your content here... -->
+	Test App
+</body>
+</html>
