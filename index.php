e9af78cab6d1a0a45e244186807818c2f8b069a2
LalynMendoza1 <lalynmendoza0@gmail.com>
2022-09-30 03:10:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,26 +5,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Test App Lalyn</title>
+	<title>Test App</title>
 
-	<script type="text/javascript" charset="utf-8">
-		(function (g, e, n, es, ys) {
-			g['_genesysJs'] = e;
-			g[e] = g[e] || function () {
-			(g[e].q = g[e].q || []).push(arguments)
-			};
-			g[e].t = 1 * new Date();
-			g[e].c = es;
-			ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-		})(window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
-			environment: 'usw2',
-			deploymentId: '8a249d75-28a2-4c9b-b779-cd2943371db1'
-		});
-	</script>
 </head>
 
 <body>
 	<!-- your content here... -->
-	Test App
+	Test App!
 </body>
 </html>
