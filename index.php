4cd58b196d55bdd64469d7bc3fc0c485b084d89e
LalynMendoza1 <lalynmendoza0@gmail.com>
2022-10-27 05:11:42 +0800

    poc humana

--- index.php
+++ index.php
@@ -6,7 +6,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<title>Test App</title>
-
+	<script type="text/javascript" charset="utf-8">
+	  (function (g, e, n, es, ys) {
+		g['_genesysJs'] = e;
+		g[e] = g[e] || function () {
+		  (g[e].q = g[e].q || []).push(arguments)
+		};
+		g[e].t = 1 * new Date();
+		g[e].c = es;
+		ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+	  })(window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
+		environment: 'usw2',
+		deploymentId: 'be2378d8-d9b3-4c7e-9a21-b2b2a48e1f1b'
+	  });
+	</script>
 </head>
 
 <body>
