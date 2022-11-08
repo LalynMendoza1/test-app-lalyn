496a008d3206bc6d614c1c7e86992d6717c903f2
LalynMendoza1 <lalynmendoza0@gmail.com>
2022-11-09 04:39:35 +0800

    update

--- index.php
+++ index.php
@@ -1,30 +1,53 @@
-<!doctype html>
+<html>
 
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <body>
 
-	<title>Test App</title>
-	<script type="text/javascript" charset="utf-8">
-	  (function (g, e, n, es, ys) {
-		g['_genesysJs'] = e;
-		g[e] = g[e] || function () {
-		  (g[e].q = g[e].q || []).push(arguments)
-		};
-		g[e].t = 1 * new Date();
-		g[e].c = es;
-		ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-	  })(window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
-		environment: 'usw2',
-		deploymentId: '8a249d75-28a2-4c9b-b779-cd2943371db1'
-	  });
-	</script>
-</head>
+   Home Office: <input type="checkbox" id="ho">
+   <br>
+   Protected Policy: <input type="checkbox" id="pp">
 
-<body>
-	<!-- your content here... -->
-	Test App!
-</body>
+    
+   </body>
 </html>
 
+<script type="text/javascript" charset="utf-8">
+
+   var ho = document.getElementById('ho');
+   var pp = document.getElementById('pp');
+   var hoValue = '';
+   var ppValue = '';
+
+
+   ho.addEventListener('change', (event) => {
+      hoValue = ho.checked;
+      Genesys("command", "Database.set", { messaging: { customAttributes: { 
+         Home_Office_Indicator: hoValue,
+         Protected_Policy: ppValue  
+      }}});
+   });
+
+   pp.addEventListener('change', (event) => {
+      ppValue = pp.checked;
+      Genesys("command", "Database.set", { messaging: { customAttributes: { 
+         Home_Office_Indicator: hoValue,
+         Protected_Policy: ppValue  
+      }}});
+   });
+
+   (function (g, e, n, es, ys) {
+      g['_genesysJs'] = e;
+      g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+      };
+      g[e].t = 1 * new Date();
+      g[e].c = es;
+      ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+   })
+   (window, 'Genesys', 'https://apps.usw2.pure.cloud/genesys-bootstrap/genesys.min.js', {
+      environment: 'usw2',
+      deploymentId: '8a249d75-28a2-4c9b-b779-cd2943371db1'
+   });
+
+
+   
+</script>
